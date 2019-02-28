b95839f54b13d64b692719658c14066fd69583b9
Farid <thesomeq@gmail.com>
2019-02-28 20:05:49 +0600

    Создана модель Movie и подключена в админку. Добавлены настройки для загрузки файлов и вывода загруженных файлов.

--- cinema/webapp/admin.py
+++ cinema/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from webapp.models import Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'release_date']
+    ordering = ['-release_date']
+    search_fields = ['name', 'id']
+
+
+admin.site.register(Movie, MovieAdmin)
