54343807d7888173a95d8d3e73ed70794e172f2d
Farid <thesomeq@gmail.com>
2019-02-28 21:59:09 +0600

    В API выведена модель Movie. Таймзона обновлена на 'Asia/Bishkek' (UTC+6).

--- cinema/cinema/urls.py
+++ cinema/cinema/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('api/v1/', include('api_v1.urls', namespace='api_v1')),
     path('admin/', admin.site.urls),
 ]
 
